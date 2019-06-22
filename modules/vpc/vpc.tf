4258e08f121e1ea62a404bcb0d947b5305e32383
Yaroslav Lysenko <lysenko.yaroslav.kharkov@gmail.com>
2019-06-22 21:56:53 +0300

    second try

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,21 +1,21 @@
 resource "aws_vpc" "main-vpc" {
   cidr_block           = var.cidr["main"]
-  enable_dns_hostnames = ${var.enable_dns_hostnames}
-  enable_dns_support   = ${var.enable_dns_support}
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
   tags = {
     Name = "MAIN_VPC"
   }
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = ${var.vpc_id}
+  vpc_id = var.vpc_id
 }
 
 resource "aws_subnet" "front_subnet" {
-  vpc_id                  = ${var.vpc_id}
+  vpc_id                  = var.vpc_id
   cidr_block              = var.cidr["public_subnet"]
-  map_public_ip_on_launch =${var.map_public_ip_on_launch}
-  availability_zone       = ${var.availability_zone}
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  availability_zone       = var.availability_zone
   depends_on              = ["aws_internet_gateway.gw"]
   tags {
     Name = "front_subnet"
